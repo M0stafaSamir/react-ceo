b2db1bfd4f79b6253fd8250562ef6c0c28b25f11
M0stafaSamir <mostafasawan966@gmail.com>
2024-08-06 06:54:25 +0300

    lightGallery

--- src/Components/Projects/FieldOfSolidWaste/Project1/Project1.jsx
+++ src/Components/Projects/FieldOfSolidWaste/Project1/Project1.jsx
@@ -3,6 +3,25 @@ import { t } from "i18next";
 import Cookies from "js-cookie";
 import { Link } from "react-router-dom";
 
+import LightGallery from "lightgallery/react";
+
+// import styles
+import "lightgallery/css/lightgallery.css";
+
+import lgZoom from "lightgallery/plugins/zoom";
+import "lightgallery/css/lg-zoom.css";
+import lgThumbnail from "lightgallery/plugins/thumbnail";
+import "lightgallery/css/lg-thumbnail.css";
+
+import lgFullScreen from "lightgallery/plugins/fullscreen";
+import "lightgallery/css/lg-fullscreen.css";
+
+import lgAutoplay from "lightgallery/plugins/autoplay";
+import "lightgallery/css/lg-autoplay.css";
+
+import lgShare from "lightgallery/plugins/share";
+import "lightgallery/css/lg-share.css";
+
 import projectOneBg from "./../../../../assets/imgs/projects/MBT in Mahalla/Picture1.jpg";
 import projectOnepic1 from "./../../../../assets/imgs/projects/MBT in Mahalla/Picture2.jpg";
 import projectOnepic2 from "./../../../../assets/imgs/projects/MBT in Mahalla/Picture3.jpg";
@@ -15,11 +34,17 @@ import projectOnepic8 from "./../../../../assets/imgs/projects/MBT in Mahalla/Pi
 import projectOnepic9 from "./../../../../assets/imgs/projects/MBT in Mahalla/Picture10.png";
 
 export default function Project1() {
+  const onInit = () => {
+    console.log("LightGallery has been initialized");
+  };
+
   const [language, setLanguage] = useState("");
   const lng = Cookies.get("i18next") || "en";
+
   useEffect(() => {
     setLanguage(lng);
   }, [lng]);
+
   return (
     <>
       <div className="projectDetailsPage">
@@ -27,13 +52,13 @@ export default function Project1() {
           <img
             className="object-fit-cover"
             src={projectOneBg}
-            alt="Design of Mechanical Biological Treatment Plant (MBT) in Mahalla city, Gharbiya, Egypt for Solid Waste Recycling "
+            alt="Design of Mechanical Biological Treatment Plant (MBT) in Mahalla city, Gharbiya, Egypt for Solid Waste Recycling"
           />
           <div
             className={
               language === "en"
-                ? "pageLocation position-absolute  bottom-0 start-0 p-3 text-white border-0 border-end border-4 mainBorderColor"
-                : "pageLocation position-absolute  bottom-0 end-0 p-3 text-white border-0 border-start border-4 mainBorderColor"
+                ? "pageLocation position-absolute bottom-0 start-0 p-3 text-white border-0 border-end border-4 mainBorderColor"
+                : "pageLocation position-absolute bottom-0 end-0 p-3 text-white border-0 border-start border-4 mainBorderColor"
             }>
             <Link className="text-white" to="/">
               {t("home")}{" "}
@@ -68,8 +93,8 @@ export default function Project1() {
         <div className="service-content py-5">
           <div className="container">
             <div className="text-center mb-5">
-              <h4 className=" fw-bold ">{t("mbtProject")}</h4>
-              <p className=" fw-medium">{t("mahalla")}</p>
+              <h4 className="fw-bold">{t("mbtProject")}</h4>
+              <p className="fw-medium">{t("mahalla")}</p>
             </div>
             <div className="row mb-5">
               <div className="col-lg-5">
@@ -104,7 +129,6 @@ export default function Project1() {
                       </tr>
                       <tr>
                         <td className="text-capitalize fw-bold px-2 text-balance py-2">
-                          {" "}
                           {t("year")}
                         </td>
                         <td>{t("project1year")}</td>
@@ -119,7 +143,7 @@ export default function Project1() {
                         <td className="text-capitalize fw-bold px-2 text-balance py-2">
                           {t("projectLocation")}
                         </td>
-                        <td> {t("mahalla")}</td>
+                        <td>{t("mahalla")}</td>
                       </tr>
                     </tbody>
                   </table>
@@ -128,154 +152,108 @@ export default function Project1() {
             </div>
             <div className="project-gallery">
               <div className="row g-4">
-                <div className="col-lg-3 co-md-6 col-sm-6 col-6">
-                  <div className="h-100 w-100 shadow">
-                    <a
-                      className="w-100 h-100"
-                      href={projectOneBg}
-                      data-fancybox="gallery"
-                      data-caption="Caption #1">
-                      <img
-                        className="w-100 h-100 object-fit-cover"
-                        src={projectOneBg}
-                      />
-                    </a>
-                  </div>
-                </div>
-                <div className="col-lg-3 co-md-6 col-sm-6 col-6">
-                  <div className="h-100 w-100 shadow">
-                    <a
-                      className="w-100 h-100"
-                      href={projectOnepic1}
-                      data-fancybox="gallery"
-                      data-caption="Caption #1">
-                      <img
-                        className="w-100 h-100 object-fit-cover"
-                        src={projectOnepic1}
-                      />
-                    </a>
-                  </div>
-                </div>
-
-                <div className="col-lg-3 co-md-6 col-sm-6 col-6">
-                  <div className="h-100 w-100 shadow">
-                    <a
-                      className="w-100 h-100"
-                      href={projectOnepic2}
-                      data-fancybox="gallery"
-                      data-caption="Caption #1">
-                      <img
-                        className="w-100 h-100 object-fit-cover"
-                        src={projectOnepic2}
-                      />
-                    </a>
-                  </div>
-                </div>
-
-                <div className="col-lg-3 co-md-6 col-sm-6 col-6">
-                  <div className="h-100 w-100 shadow">
-                    <a
-                      className="w-100 h-100"
-                      href={projectOnepic3}
-                      data-fancybox="gallery"
-                      data-caption="Caption #1">
-                      <img
-                        className="w-100 h-100 object-fit-cover"
-                        src={projectOnepic3}
-                      />
-                    </a>
-                  </div>
-                </div>
-
-                <div className="col-lg-3 co-md-6 col-sm-6 col-6">
-                  <div className="h-100 w-100 shadow">
-                    <a
-                      className="w-100 h-100"
-                      href={projectOnepic4}
-                      data-fancybox="gallery"
-                      data-caption="Caption #1">
-                      <img
-                        className="w-100 h-100 object-fit-cover"
-                        src={projectOnepic4}
-                      />
-                    </a>
-                  </div>
-                </div>
-
-                <div className="col-lg-3 co-md-6 col-sm-6 col-6">
-                  <div className="h-100 w-100 shadow">
-                    <a
-                      className="w-100 h-100"
-                      href={projectOnepic5}
-                      data-fancybox="gallery"
-                      data-caption="Caption #1">
-                      <img
-                        className="w-100 h-100 object-fit-cover"
-                        src={projectOnepic5}
-                      />
-                    </a>
-                  </div>
-                </div>
-
-                <div className="col-lg-3 co-md-6 col-sm-6 col-6">
-                  <div className="h-100 w-100 shadow">
-                    <a
-                      className="w-100 h-100"
-                      href={projectOnepic6}
-                      data-fancybox="gallery"
-                      data-caption="Caption #1">
-                      <img
-                        className="w-100 h-100 object-fit-cover"
-                        src={projectOnepic6}
-                      />
-                    </a>
-                  </div>
-                </div>
-
-                <div className="col-lg-3 co-md-6 col-sm-6 col-6">
-                  <div className="h-100 w-100 shadow">
-                    <a
-                      className="w-100 h-100"
-                      href={projectOnepic7}
-                      data-fancybox="gallery"
-                      data-caption="Caption #1">
-                      <img
-                        className="w-100 h-100 object-fit-cover"
-                        src={projectOnepic7}
-                      />
-                    </a>
-                  </div>
-                </div>
-
-                <div className="col-lg-3 co-md-6 col-sm-6 col-6">
-                  <div className="h-100 w-100 shadow">
-                    <a
-                      className="w-100 h-100"
-                      href={projectOnepic8}
-                      data-fancybox="gallery"
-                      data-caption="Caption #1">
-                      <img
-                        className="w-100 h-100 object-fit-cover"
-                        src={projectOnepic8}
-                      />
-                    </a>
-                  </div>
-                </div>
-
-                <div className="col-lg-3 co-md-6 col-sm-6 col-6">
-                  <div className="h-100 w-100 shadow">
-                    <a
-                      className="w-100 h-100"
-                      href={projectOnepic9}
-                      data-fancybox="gallery"
-                      data-caption="Caption #1">
-                      <img
-                        className="w-100 h-100 object-fit-cover"
-                        src={projectOnepic9}
-                      />
-                    </a>
-                  </div>
-                </div>
+                <LightGallery
+                  className="row d-flex"
+                  onInit={onInit}
+                  speed={500}
+                  plugins={[
+                    lgThumbnail,
+                    lgZoom,
+                    lgFullScreen,
+                    lgAutoplay,
+                    lgShare,
+                  ]}>
+                  <a
+                    className="col-6 col-sm-6 col-md-4 col-lg-3 galleryAtag p-2"
+                    href={projectOneBg}>
+                    <img
+                      className="w-100 h-100 object-fit-cover"
+                      src={projectOneBg}
+                      alt="Gallery Image"
+                    />
+                  </a>
+                  <a
+                    className="col-6 col-sm-6 col-md-4 col-lg-3 galleryAtag p-2"
+                    href={projectOnepic1}>
+                    <img
+                      className="w-100 h-100 object-fit-cover"
+                      src={projectOnepic1}
+                      alt="Gallery Image"
+                    />
+                  </a>
+                  <a
+                    className=" col-6 col-sm-6 col-md-4 col-lg-3 galleryAtag p-2"
+                    href={projectOnepic2}>
+                    <img
+                      className="w-100 h-100 object-fit-cover"
+                      src={projectOnepic2}
+                      alt="Gallery Image"
+                    />
+                  </a>
+                  <a
+                    className="col-6 col-sm-6 col-md-4 col-lg-3 galleryAtag p-2"
+                    href={projectOnepic3}>
+                    <img
+                      className="w-100 h-100 object-fit-cover"
+                      src={projectOnepic3}
+                      alt="Gallery Image"
+                    />
+                  </a>
+                  <a
+                    className="col-6 col-sm-6 col-md-4 col-lg-3 galleryAtag p-2"
+                    href={projectOnepic4}>
+                    <img
+                      className="w-100 h-100 object-fit-cover"
+                      src={projectOnepic4}
+                      alt="Gallery Image"
+                    />
+                  </a>
+                  <a
+                    className="col-6 col-sm-6 col-md-4 col-lg-3 galleryAtag p-2"
+                    href={projectOnepic5}>
+                    <img
+                      className="w-100 h-100 object-fit-cover"
+                      src={projectOnepic5}
+                      alt="Gallery Image"
+                    />
+                  </a>
+                  <a
+                    className="col-6 col-sm-6 col-md-4 col-lg-3 galleryAtag p-2"
+                    href={projectOnepic6}>
+                    <img
+                      className="w-100 h-100 col-lg-3 object-fit-cover"
+                      src={projectOnepic6}
+                      alt="Gallery Image"
+                    />
+                  </a>
+                  <a
+                    className="col-6 col-sm-6 col-md-4 col-lg-3 galleryAtag p-2"
+                    href={projectOnepic7}>
+                    <img
+                      className="w-100 h-100 object-fit-cover"
+                      src={projectOnepic7}
+                      alt="Gallery Image"
+                    />
+                  </a>
+                  <a
+                    className="col-6 col-sm-6 col-md-4 col-lg-3 galleryAtag p-2"
+                    href={projectOnepic8}>
+                    <img
+                      className="w-100 h-100 object-fit-cover"
+                      src={projectOnepic8}
+                      alt="Gallery Image"
+                    />
+                  </a>
+                  <a
+                    className="col-6 col-sm-6 col-md-4 col-lg-3 galleryAtag p-2"
+                    href={projectOnepic9}>
+                    <img
+                      className="w-100 h-100 object-fit-cover"
+                      src={projectOnepic9}
+                      alt="Gallery Image"
+                    />
+                  </a>
+                </LightGallery>
               </div>
             </div>
           </div>
